3ae3f5df6be9fce1e96c98bee76c87de4e35e95d
bolein <bolein95@gmail.com>
2018-08-14 11:04:53 -0500

    fixed not serializable filters

--- library/src/main/java/eu/theappshop/baseadapter/misc/Filter.java
+++ library/src/main/java/eu/theappshop/baseadapter/misc/Filter.java
@@ -1,8 +1,8 @@
 package eu.theappshop.baseadapter.misc;
 
+import java.io.Serializable;
 import java.util.List;
 
-public interface Filter<T> {
-
+public interface Filter<T> extends Serializable {
   List<T> filter(List<T> list);
 }
