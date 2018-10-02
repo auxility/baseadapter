3666437a26260c336f592e5987b0b88d1af428ef
bolein <bolein95@gmail.com>
2018-10-02 00:21:53 -0500

    Cart API

--- library/src/main/java/eu/theappshop/baseadapter/vm/DiffVM.java
+++ library/src/main/java/eu/theappshop/baseadapter/vm/DiffVM.java
@@ -1,9 +1,11 @@
 package eu.theappshop.baseadapter.vm;
 
+import android.support.annotation.NonNull;
+
 public interface DiffVM extends VM {
 
-  boolean isEqualItem(VM vm);
+  boolean isEqualItem(@NonNull VM vm);
 
-  boolean isEqualContent(VM vm);
+  boolean isEqualContent(@NonNull VM vm);
 
 }
