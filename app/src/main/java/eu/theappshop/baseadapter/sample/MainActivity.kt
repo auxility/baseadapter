eebde7e98064416bc605c33fd25be2b8016730ea
skiff2011 <alekseismovzhenko@gmail.com>
2019-03-06 16:25:46 +0200

    Merge branch 'skiff2011/sample' of github.com:skiff2011/baseadapter into skiff2011/refactor
    # Conflicts:
    #	app/build.gradle
    #	build.gradle
    #	gradle/wrapper/gradle-wrapper.properties

--- app/src/main/java/eu/theappshop/baseadapter/sample/MainActivity.kt
+++ app/src/main/java/eu/theappshop/baseadapter/sample/MainActivity.kt
@@ -1,13 +1,13 @@
 package eu.theappshop.baseadapter.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import eu.theappshop.baseadapterproject.R
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
 }
